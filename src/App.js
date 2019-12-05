2e488c2bd97a7c61f3849a89276a9d5b14267942
Mariam Ali <mariamqali13@gmail.com>
2019-12-05 18:35:02 +0300

    removed everything except for dummy data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-
-//Components
-import ShowDetail from "./Components/ShowDetail";
-import HomePage from "./Components/HomePage";
-import TopNavBar from "./Components/TopNavBar";
-import EpisodeDetail from "./Components/EpisodeDetail";
-
-//Data
-import shows from "./Data/shows";
-import episodes from "./Data/episodes";
 
 function App() {
-  return (
-    <div>
-      <TopNavBar />
-
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route
-          path="/show/:showID"
-          render={props => <ShowDetail {...props} shows={shows} />}
-        />
-        <Route
-          path="/episode/:episodeID"
-          render={props => <EpisodeDetail {...props} episodes={episodes} />}
-        />
-        <Redirect to="/" />
-      </Switch>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
 
-//Design
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
